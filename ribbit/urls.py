f5fdbeb945e74ff70e651b26b892379cda18c69e
Ahmed Mubbashir Khan <mubbashir11@gmail.com>
2013-02-16 21:44:36 +0400

    Implemented Ribbit Submission and Public Ribbits Views

--- ribbit/urls.py
+++ ribbit/urls.py
@@ -18,4 +18,6 @@ urlpatterns = patterns('',
     url(r'^login$', 'ribbit_app.views.login_view'), # login
     url(r'^logout$', 'ribbit_app.views.logout_view'), # logout
     url(r'^signup$', 'ribbit_app.views.signup'), # signup
+    url(r'^ribbits$', 'ribbit_app.views.public'), # public ribbits
+    url(r'^submit$', 'ribbit_app.views.submit'), # submit new ribbit
 )
